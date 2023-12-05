b1818bb76741c2ec786df6cf0219c7ea628a7709
julialozmin <julialozmin@gmail.com>
2023-12-05 11:55:02 +0100

    Installed Bootstrap instead of having the link

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <div className="container">
+        <Card />
 
-      <small>
-        Coded by{" "}
-        <a href="http://www.linkedin.com/in/julia-lozoya-25a400146">
-          Julia Lozoya
-        </a>
-        , available open source on{" "}
-        <a href="https://github.com/julialozmin/ReactWeatherApp.git">GitHub</a>{" "}
-        and hosted on <a href="https://www.netlify.com/">Netlify</a>
-      </small>
+        <small>
+          Coded by{" "}
+          <a href="http://www.linkedin.com/in/julia-lozoya-25a400146">
+            Julia Lozoya
+          </a>
+          , available open source on{" "}
+          <a href="https://github.com/julialozmin/ReactWeatherApp.git">
+            GitHub
+          </a>{" "}
+          and hosted on <a href="https://www.netlify.com/">Netlify</a>
+        </small>
+      </div>
     </div>
   );
 }
