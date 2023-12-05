656d29277c2f46e522da03fee364872840b880cd
julialozmin <julialozmin@gmail.com>
2023-12-05 12:00:08 +0100

    Updated CSS and removed default sets

--- src/Card.js
+++ src/Card.js
@@ -9,33 +9,29 @@ import Forecast from "./Forecast";
 export default function Card() {
   return (
     <div className="Card">
-      <div className="container">
-        <div className="cardStructure">
+      <div className="row">
+        <div className="col-12">
+          <Form />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-8">
           <div className="row">
             <div className="col-12">
-              <Form />
+              <DateHour />
             </div>
           </div>
           <div className="row">
-            <div className="col-8">
-              <div className="row">
-                <div className="col-12">
-                  <DateHour />
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-6">
-                  <CityDetails />
-                </div>
-                <div className="col-6">
-                  <TemperatureDetails />
-                </div>
-              </div>
+            <div className="col-6">
+              <CityDetails />
             </div>
-            <div className="col-4">
-              <Forecast />
+            <div className="col-6">
+              <TemperatureDetails />
             </div>
           </div>
+        </div>
+        <div className="col-4">
+          <Forecast />
         </div>
       </div>
     </div>
